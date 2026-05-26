b4696c2defa43f347d1616bce3dad435bda3e038
김명근 <rlaaudrms369@gmail.com>
2026-05-26 19:22:07 +0900

    test: 정적 팩토리 메서드, 좋아요 증가 감소 메서드 테스트 메서드 작성

---
+++ src/test/java/com/sprint/mission/monew/domain/comment/entity/CommentTest.java
@@ -0,0 +1,66 @@
+package com.sprint.mission.monew.domain.comment.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.sprint.mission.monew.domain.article.entity.Article;
+import com.sprint.mission.monew.domain.user.entity.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class CommentTest {
+
+    @Nested
+    @DisplayName("댓글 생성하기")
+    class Create {
+
+        @Test
+        @DisplayName("댓글 생성")
+        void 댓글_생성() {
+            // given
+            Article article = new Article();
+            User user = new User();
+            String content = "댓글 내용";
+
+            // when
+            Comment comment = Comment.create(article, user, content);
+
+            // then
+            assertThat(comment.getArticle()).isEqualTo(article);
+            assertThat(comment.getUser()).isEqualTo(user);
+            assertThat(comment.getContent()).isEqualTo(content);
+        }
+    }
+
+    @Nested
+    @DisplayName("좋아요")
+    class Like {
+
+        @Test
+        @DisplayName("좋아요 증가")
+        void 좋아요_증가() {
+            // given
+            Comment comment = Comment.create(new Article(), new User(), "댓글 내용");
+
+            // when
+            comment.increaseLikeCount();
+
+            // then
+            assertThat(comment.getLikeCount()).isEqualTo(1);
+        }
+
+        @Test
+        @DisplayName("좋아요 감소")
+        void 좋아요_감소() {
+            // given
+            Comment comment = Comment.create(new Article(), new User(), "댓글 내용");
+            comment.increaseLikeCount(); // 미리 1 증가시키기
+
+            // when
+            comment.decreaseLikeCount();
+
+            // then
+            assertThat(comment.getLikeCount()).isEqualTo(0);
+        }
+    }
+}
