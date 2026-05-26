ccb4e345f9d2b2fa712c317f0462adffc86a9ae8
김명근 <rlaaudrms369@gmail.com>
2026-05-26 14:29:33 +0900

    feat: 실패 테스트를 위한 null을 반환하는 create 메서드 작성

---
+++ src/main/java/com/sprint/mission/monew/domain/comment/service/CommentService.java
@@ -0,0 +1,10 @@
+package com.sprint.mission.monew.domain.comment.service;
+
+import com.sprint.mission.monew.domain.comment.dto.request.CommentCreateRequest;
+import com.sprint.mission.monew.domain.comment.dto.response.CommentResponse;
+
+public class CommentService {
+    public CommentResponse create(CommentCreateRequest request) {
+        return null;
+    }
+}
