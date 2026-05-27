7d2e6111e6991c98bf09472cb45192019faec110
김명근 <rlaaudrms369@gmail.com>
2026-05-27 11:03:20 +0900

    test(green): 댓글 등록 메서드 작성

--- src/main/java/com/sprint/mission/monew/domain/comment/controller/CommentController.java
+++ src/main/java/com/sprint/mission/monew/domain/comment/controller/CommentController.java
@@ -3,7 +3,10 @@ package com.sprint.mission.monew.domain.comment.controller;
 import com.sprint.mission.monew.domain.comment.controller.api.CommentApi;
 import com.sprint.mission.monew.domain.comment.dto.request.CommentCreateRequest;
 import com.sprint.mission.monew.domain.comment.dto.response.CommentResponse;
+import com.sprint.mission.monew.domain.comment.service.CommentService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,12 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/comments")
 public class CommentController implements CommentApi {
 
+    private final CommentService commentService;
+
     @Override
     @PostMapping
-    public ResponseEntity<CommentResponse> createComment(@RequestBody @Valid CommentCreateRequest request) {
-        return ResponseEntity.status(null).body(null);
+    public ResponseEntity<CommentResponse> createComment(@RequestBody CommentCreateRequest request) {
+
+        CommentResponse response = commentService.create(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
