c3b072f978b433db20661599889bb93054e13cd1
김명근 <rlaaudrms369@gmail.com>
2026-05-27 11:18:59 +0900

    refactor: 테스트 공통 변수를 BeforeEach로 분리하여 초기화

--- src/test/java/com/sprint/mission/monew/domain/comment/entity/CommentTest.java
+++ src/test/java/com/sprint/mission/monew/domain/comment/entity/CommentTest.java
@@ -4,12 +4,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sprint.mission.monew.domain.article.entity.Article;
 import com.sprint.mission.monew.domain.user.entity.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class CommentTest {
 
+    private Article article;
+    private User user;
+    private String content;
+    private Comment comment;
+
+    @BeforeEach
+    void setUp() {
+        article = new Article();
+        user = new User();
+        content = "댓글 내용";
+
+        comment = Comment.create(article, user, content);
+    }
     @Nested
     @DisplayName("댓글 생성하기")
     class Create {
@@ -18,12 +32,10 @@ public class CommentTest {
         @DisplayName("댓글 생성")
         void 댓글_생성() {
             // given
-            Article article = new Article();
-            User user = new User();
-            String content = "댓글 내용";
+            // setUp()의 article, user, content 초기화
 
             // when
-            Comment comment = Comment.create(article, user, content);
+            // setUp()의 comment 초기화
 
             // then
             assertThat(comment.getArticle()).isEqualTo(article);
@@ -40,7 +52,7 @@ public class CommentTest {
         @DisplayName("좋아요 증가")
         void 좋아요_증가() {
             // given
-            Comment comment = Comment.create(new Article(), new User(), "댓글 내용");
+            // setUp()의 comment 초기화
 
             // when
             comment.increaseLikeCount();
@@ -53,7 +65,7 @@ public class CommentTest {
         @DisplayName("좋아요 감소")
         void 좋아요_감소() {
             // given
-            Comment comment = Comment.create(new Article(), new User(), "댓글 내용");
+            // setUp()의 comment 초기화
             comment.increaseLikeCount(); // 미리 1 증가시키기
 
             // when
