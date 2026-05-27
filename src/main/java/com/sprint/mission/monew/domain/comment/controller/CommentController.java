ee5ef5de874cf237dfc9d623eb9ff1c5030f26b1
김명근 <rlaaudrms369@gmail.com>
2026-05-27 22:07:25 +0900

    feat: 실패 테스트를 위한 updateComment() Stub 메서드 작성

--- src/main/java/com/sprint/mission/monew/domain/comment/controller/CommentController.java
+++ src/main/java/com/sprint/mission/monew/domain/comment/controller/CommentController.java
@@ -2,15 +2,20 @@ package com.sprint.mission.monew.domain.comment.controller;
 
 import com.sprint.mission.monew.domain.comment.controller.api.CommentApi;
 import com.sprint.mission.monew.domain.comment.dto.request.CommentCreateRequest;
+import com.sprint.mission.monew.domain.comment.dto.request.CommentUpdateRequest;
 import com.sprint.mission.monew.domain.comment.dto.response.CommentResponse;
 import com.sprint.mission.monew.domain.comment.service.CommentService;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +39,15 @@ public class CommentController implements CommentApi {
     log.debug("[COMMENT_CREATE_RESPONSE] 댓글 생성 응답 - 댓글 ID={}", response.id());
 
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
+
+  @Override
+  @PatchMapping("/{commentId}")
+  public ResponseEntity<CommentResponse> updateComment(
+      @PathVariable UUID commentId,
+      @RequestHeader("Monew-Request-User-ID") UUID userId,
+      @RequestBody CommentUpdateRequest request
+  ) {
+    return ResponseEntity.status(null).body(null);
   }
 }
--- src/main/java/com/sprint/mission/monew/domain/comment/controller/api/CommentApi.java
+++ src/main/java/com/sprint/mission/monew/domain/comment/controller/api/CommentApi.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.monew.domain.comment.controller.api;
 
 import com.sprint.mission.monew.domain.comment.dto.request.CommentCreateRequest;
+import com.sprint.mission.monew.domain.comment.dto.request.CommentUpdateRequest;
 import com.sprint.mission.monew.domain.comment.dto.response.CommentResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -8,8 +9,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "댓글 관리", description = "댓글 관련 API")
 public interface CommentApi {
@@ -21,4 +25,9 @@ public interface CommentApi {
       @ApiResponse(responseCode = "500", description = "서버 내부 오류")
   })
   ResponseEntity<CommentResponse> createComment(@RequestBody @Valid CommentCreateRequest request);
+
+  ResponseEntity<CommentResponse> updateComment(
+      @PathVariable UUID commentId,
+      @RequestHeader("Monew-Request-User-ID") UUID userId,
+      @RequestBody CommentUpdateRequest request);
 }
