25c78208b63899bcfc7291240bdb6aa47c68295d
김명근 <rlaaudrms369@gmail.com>
2026-05-27 13:43:43 +0900

    fix: 잘못된 문법 올바르게 수정

--- src/main/java/com/sprint/mission/monew/domain/comment/mapper/CommentMapper.java
+++ src/main/java/com/sprint/mission/monew/domain/comment/mapper/CommentMapper.java
@@ -8,8 +8,8 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
     @Mapping(target = "articleId", expression = "java(comment.getArticle().getId())")
-    @Mapping(target = "userId", expression = "java(comment.getUser() != null ? comment.getUser().getId()) : null")
-    @Mapping(target = "userNickname", expression = "java(comment.getUser() != null ? comment.getUser().getNickname()) : \"알 수 없음\"")
+    @Mapping(target = "userId", expression = "java(comment.getUser() != null ? comment.getUser().getId() : null)")
+    @Mapping(target = "userNickname", expression = "java(comment.getUser() != null ? comment.getUser().getNickname() : \"알 수 없음\")")
     @Mapping(target = "likedByMe", source = "likedByMe")
-    CommentResponse toDto(Comment comment, boolean likedByMe);
+    CommentResponse toResponse(Comment comment, boolean likedByMe);
 }
