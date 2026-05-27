917644eb0ec9b9ca70253a8e039b98206bc16e00
김명근 <rlaaudrms369@gmail.com>
2026-05-27 12:05:23 +0900

    chore: CommentApi(Swagger 문서) 작성

--- src/main/java/com/sprint/mission/monew/domain/comment/controller/api/CommentApi.java
+++ src/main/java/com/sprint/mission/monew/domain/comment/controller/api/CommentApi.java
@@ -2,8 +2,20 @@ package com.sprint.mission.monew.domain.comment.controller.api;
 
 import com.sprint.mission.monew.domain.comment.dto.request.CommentCreateRequest;
 import com.sprint.mission.monew.domain.comment.dto.response.CommentResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "댓글 관리", description = "댓글 관련 API")
 public interface CommentApi {
-    ResponseEntity<CommentResponse> createComment(CommentCreateRequest request);
+    @Operation(summary = "댓글 등록", description = "새로운 댓글을 등록합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "등록 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 (입력값 검증 실패)"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
+    ResponseEntity<CommentResponse> createComment(@Parameter CommentCreateRequest request);
 }
