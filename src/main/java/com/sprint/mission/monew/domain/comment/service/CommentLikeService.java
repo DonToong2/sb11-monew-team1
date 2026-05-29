d88e5fbc7745ec5e53ca7d410ee422666bf06f56
김명근 <rlaaudrms369@gmail.com>
2026-05-29 22:45:30 +0900

    refactor: 잘못된 로깅 메시지 수정

--- src/main/java/com/sprint/mission/monew/domain/comment/service/CommentLikeService.java
+++ src/main/java/com/sprint/mission/monew/domain/comment/service/CommentLikeService.java
@@ -75,7 +75,7 @@ public class CommentLikeService {
     commentLikeRepository.deleteByUserIdAndCommentId(userId, commentId);
     commentRepository.decreaseLikeCount(commentId);
 
-    log.info("[COMMENT_LIKE_CANCEL_SUCCESS] 댓글 좋아요 등록 성공 - 요청자 ID={}, 댓글 ID={}",
+    log.info("[COMMENT_LIKE_CANCEL_SUCCESS] 댓글 좋아요 취소 성공 - 요청자 ID={}, 댓글 ID={}",
         userId, commentId);
   }
 
