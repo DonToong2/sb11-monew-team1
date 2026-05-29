b7d3bd12287a99a317067d200a873513060fd7c7
김명근 <rlaaudrms369@gmail.com>
2026-05-29 09:30:41 +0900

    test(red): 정상적인 hardDelete() 테스트 메서드 작성

--- src/test/java/com/sprint/mission/monew/domain/comment/service/CommentServiceTest.java
+++ src/test/java/com/sprint/mission/monew/domain/comment/service/CommentServiceTest.java
@@ -254,4 +254,23 @@ public class CommentServiceTest {
       assertThat(comment.isDeleted()).isTrue();
     }
   }
+
+  @Nested
+  @DisplayName("댓글 물리 삭제하기")
+  class Service_Comment_HardDelete {
+
+    @Test
+    @DisplayName("댓글 물리삭제 성공")
+    void 댓글_물리삭제_성공() {
+      // given
+      Comment comment = Comment.create(article, user, content);
+      given(commentRepository.findById(commentId)).willReturn(Optional.of(comment));
+
+      // when
+      commentService.hardDelete(commentId);
+
+      // then
+      verify(commentRepository).delete(comment);
+    }
+  }
 }
