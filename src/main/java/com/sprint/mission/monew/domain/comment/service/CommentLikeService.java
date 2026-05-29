22b66002a1af665df0b29af4723f07879445818c
김명근 <rlaaudrms369@gmail.com>
2026-05-29 16:27:06 +0900

    test(green): create() 메서드 예외처리 구현

--- src/main/java/com/sprint/mission/monew/domain/comment/service/CommentLikeService.java
+++ src/main/java/com/sprint/mission/monew/domain/comment/service/CommentLikeService.java
@@ -3,10 +3,13 @@ package com.sprint.mission.monew.domain.comment.service;
 import com.sprint.mission.monew.domain.comment.dto.response.CommentLikeResponse;
 import com.sprint.mission.monew.domain.comment.entity.Comment;
 import com.sprint.mission.monew.domain.comment.entity.CommentLike;
+import com.sprint.mission.monew.domain.comment.exception.CommentLikeAlreadyExistsException;
+import com.sprint.mission.monew.domain.comment.exception.CommentNotFoundException;
 import com.sprint.mission.monew.domain.comment.mapper.CommentLikeMapper;
 import com.sprint.mission.monew.domain.comment.repository.CommentLikeRepository;
 import com.sprint.mission.monew.domain.comment.repository.CommentRepository;
 import com.sprint.mission.monew.domain.user.entity.User;
+import com.sprint.mission.monew.domain.user.exception.UserNotFoundException;
 import com.sprint.mission.monew.domain.user.repository.UserRepository;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -25,8 +28,18 @@ public class CommentLikeService {
 
   @Transactional
   public CommentLikeResponse create(UUID commentId, UUID userId) {
-    User user = userRepository.findById(userId).orElseThrow();
-    Comment comment = commentRepository.findById(commentId).orElseThrow();
+
+    // 사용자가 이미 댓글에 좋아요를 눌렀다면 예외처리
+    if (commentLikeRepository.existsByUserIdAndCommentId(userId, commentId)) {
+      throw CommentLikeAlreadyExistsException.withId(userId, commentId);
+    }
+
+    User user = userRepository.findById(userId).orElseThrow(
+        () -> UserNotFoundException.withId(userId)
+    );
+    Comment comment = commentRepository.findById(commentId).orElseThrow(
+        () -> CommentNotFoundException.withId(commentId)
+    );
 
     CommentLike commentLike = CommentLike.create(user, comment);
 
