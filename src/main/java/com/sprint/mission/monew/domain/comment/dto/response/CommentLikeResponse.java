0083b8e6bb5bd6566e4da117ad16836f5c9031d6
김명근 <rlaaudrms369@gmail.com>
2026-05-30 23:04:55 +0900

    fix: 좋아요 수(likeCount) int → long으로 변경

--- src/main/java/com/sprint/mission/monew/domain/comment/dto/response/CommentLikeResponse.java
+++ src/main/java/com/sprint/mission/monew/domain/comment/dto/response/CommentLikeResponse.java
@@ -12,7 +12,7 @@ public record CommentLikeResponse(
     UUID commentUserId,
     String commentUserNickname,
     String commentContent,
-    int commentLikeCount,
+    long commentLikeCount,
     Instant commentCreatedAt
 ) {
 
