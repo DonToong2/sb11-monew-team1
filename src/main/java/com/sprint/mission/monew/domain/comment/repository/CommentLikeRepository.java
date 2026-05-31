d5623335ee4e5d82ffbdd711723bc215d92eee69
김명근 <rlaaudrms369@gmail.com>
2026-06-01 02:13:52 +0900

    feat: 댓글 수만큼 좋아요 수가 조회되는 N+1 문제를 해결하기 위한 findLikedCommentIds Stub 메서드 작성

--- src/main/java/com/sprint/mission/monew/domain/comment/repository/CommentLikeRepository.java
+++ src/main/java/com/sprint/mission/monew/domain/comment/repository/CommentLikeRepository.java
@@ -1,12 +1,17 @@
 package com.sprint.mission.monew.domain.comment.repository;
 
 import com.sprint.mission.monew.domain.comment.entity.CommentLike;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike, UUID> {
 
   boolean existsByUserIdAndCommentId(UUID userId, UUID commentId);
+  
+  Set<UUID> findLikedCommentIds(UUID userId, List<UUID> commentIds);
 
   int deleteByUserIdAndCommentId(UUID userId, UUID commentId);
 }
