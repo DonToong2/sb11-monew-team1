62f9e4530e5ee1aec1fd3a97e5ef25d21ef96ac5
김명근 <rlaaudrms369@gmail.com>
2026-06-02 19:41:01 +0900

    test(green): cleanUpExpiredNotifications() 메서드 구현

--- src/main/java/com/sprint/mission/monew/domain/notification/scheduler/NotificationCleanupScheduler.java
+++ src/main/java/com/sprint/mission/monew/domain/notification/scheduler/NotificationCleanupScheduler.java
@@ -1,9 +1,12 @@
 package com.sprint.mission.monew.domain.notification.scheduler;
 
 import com.sprint.mission.monew.domain.notification.service.NotificationService;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,8 +22,13 @@ public class NotificationCleanupScheduler {
   private final Job notificationDeleteJob;
 
   @Scheduled(cron = "${scheduler.notification-cleanup.cron}")
-  public void cleanUpExpiredNotifications() {
+  public void cleanUpExpiredNotifications() throws Exception {
     log.debug("만료 알림 삭제 스케줄러 실행");
+
+    JobParameters params = new JobParametersBuilder()
+        .addLong("time", Instant.now().toEpochMilli()).toJobParameters();
+
+    jobLauncher.run(notificationDeleteJob, params);
 
     log.info("만료 알림 삭제 완료");
   }
