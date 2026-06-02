6593c42cb88d1e609b095b74163ccf1767369f94
김명근 <rlaaudrms369@gmail.com>
2026-06-02 13:56:09 +0900

    chore: 대량의 유저 삭제를 대비한 인덱스 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS users
 CREATE INDEX IF NOT EXISTS idx_users_deleted_at ON users (deleted_at)
     WHERE deleted_at IS NULL;
 
+CREATE INDEX IF NOT EXISTS idx_users_deleted_at_batch ON users (deleted_at);
 
 -- =====================
 -- 2. interests
