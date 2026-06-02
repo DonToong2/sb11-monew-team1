1570fcd73abc18e915b956a2dc5604a80a14c4e1
김명근 <rlaaudrms369@gmail.com>
2026-06-02 19:14:19 +0900

    refactor: 로깅 메시지 복구

--- src/main/java/com/sprint/mission/monew/batch/UserDeleteTasklet.java
+++ src/main/java/com/sprint/mission/monew/batch/UserDeleteTasklet.java
@@ -4,12 +4,14 @@ import com.sprint.mission.monew.domain.user.repository.UserRepository;
 import java.time.Duration;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserDeleteTasklet implements Tasklet {
@@ -18,9 +20,14 @@ public class UserDeleteTasklet implements Tasklet {
 
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
+
     Instant threshold = Instant.now().minus(Duration.ofDays(1));
 
-    userRepository.deleteAllByDeletedAtBefore(threshold);
+    log.info("물리 삭제 실행: 기준시점={}", threshold);
+
+    int deleted = userRepository.deleteAllByDeletedAtBefore(threshold);
+
+    log.info("물리 삭제 완료: {}건 삭제", deleted);
 
     return RepeatStatus.FINISHED;
 
