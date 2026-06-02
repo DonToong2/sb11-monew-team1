0247cca600868227c8ece9033378e2cef630e34b
김명근 <rlaaudrms369@gmail.com>
2026-06-02 19:31:51 +0900

    refactor: job, step 기반 테스트를 위한 Stub 메서드로 변경

--- src/main/java/com/sprint/mission/monew/domain/notification/scheduler/NotificationCleanupScheduler.java
+++ src/main/java/com/sprint/mission/monew/domain/notification/scheduler/NotificationCleanupScheduler.java
@@ -3,6 +3,8 @@ package com.sprint.mission.monew.domain.notification.scheduler;
 import com.sprint.mission.monew.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,12 +15,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificationCleanupScheduler {
 
-  private final NotificationService notificationService;
+  private final JobLauncher jobLauncher;
+  private final Job notificationDeleteJob;
 
   @Scheduled(cron = "${scheduler.notification-cleanup.cron}")
   public void cleanUpExpiredNotifications() {
     log.debug("만료 알림 삭제 스케줄러 실행");
-    notificationService.deleteExpiredNotifications();
+
     log.info("만료 알림 삭제 완료");
   }
 }
