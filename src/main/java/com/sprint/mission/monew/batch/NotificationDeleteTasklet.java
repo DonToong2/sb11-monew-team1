6d8a4d157744dfd298900e51dfe007dc0c7e2cbd
김명근 <rlaaudrms369@gmail.com>
2026-06-02 19:15:06 +0900

    test(green): execute() 메서드 구현

--- src/main/java/com/sprint/mission/monew/batch/NotificationDeleteTasklet.java
+++ src/main/java/com/sprint/mission/monew/batch/NotificationDeleteTasklet.java
@@ -1,13 +1,17 @@
 package com.sprint.mission.monew.batch;
 
 import com.sprint.mission.monew.domain.notification.repository.NotificationRepository;
+import java.time.Duration;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class NotificationDeleteTasklet implements Tasklet {
@@ -16,7 +20,16 @@ public class NotificationDeleteTasklet implements Tasklet {
 
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
-    throw new UnsupportedOperationException();
+
+    Instant cutoff = Instant.now().minus(Duration.ofDays(7));
+
+    log.info("만료 알림 삭제 실행: 기준시점={}", cutoff);
+
+    int deleted = notificationRepository.deleteConfirmedBefore(cutoff);
+
+    log.info("만료 알림 삭제 완료: {}건 삭제", deleted);
+
+    return RepeatStatus.FINISHED;
   }
 
 }
