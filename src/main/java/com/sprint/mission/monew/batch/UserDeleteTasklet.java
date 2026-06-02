024437654e99b56e9352252a1de3f6d0cc539e0c
김명근 <rlaaudrms369@gmail.com>
2026-06-02 15:10:23 +0900

    test(green): execute() 메서드 구현

--- src/main/java/com/sprint/mission/monew/batch/UserDeleteTasklet.java
+++ src/main/java/com/sprint/mission/monew/batch/UserDeleteTasklet.java
@@ -1,5 +1,9 @@
 package com.sprint.mission.monew.batch;
 
+import com.sprint.mission.monew.domain.user.repository.UserRepository;
+import java.time.Duration;
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -7,11 +11,19 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserDeleteTasklet implements Tasklet {
+
+  private final UserRepository userRepository;
 
   @Override
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
-    throw new UnsupportedOperationException();
+    Instant threshold = Instant.now().minus(Duration.ofDays(1));
+
+    userRepository.deleteAllByDeletedAtBefore(threshold);
+
+    return RepeatStatus.FINISHED;
+
   }
 
 
