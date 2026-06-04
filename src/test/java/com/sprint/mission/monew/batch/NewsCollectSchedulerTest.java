7e2af5785eac7d6d7be2a5371dfefe5fbf9aa776
김명근 <rlaaudrms369@gmail.com>
2026-06-04 12:11:24 +0900

    test(red): 정상적인 collect() 테스트 메서드 작성

--- src/test/java/com/sprint/mission/monew/batch/NewsCollectSchedulerTest.java
+++ src/test/java/com/sprint/mission/monew/batch/NewsCollectSchedulerTest.java
@@ -1,33 +1,54 @@
 package com.sprint.mission.monew.batch;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
 
 @ExtendWith(MockitoExtension.class)
 class NewsCollectSchedulerTest {
 
-  @InjectMocks NewsCollectScheduler newsCollectScheduler;
-  @Mock NewsCollectService newsCollectService;
+  @Mock
+  private JobLauncher jobLauncher;
 
+  @Mock
+  private Job newsCollectJob;
+
+  private NewsCollectScheduler scheduler;
+
+  @BeforeEach
+  void setUp() {
+    scheduler = new NewsCollectScheduler(jobLauncher, newsCollectJob);
+  }
   @Nested
   @DisplayName("뉴스 수집 스케줄러")
   class Collect {
 
     @Test
-    @DisplayName("collect 호출 시 NewsCollectService에 위임한다")
-    void collect_호출_시_서비스에_위임한다() {
+    @DisplayName("스케줄러가 Batch Job을 호출한다")
+    void 스케줄러가_Batch_Job의_collect_메서드를_호출한다() throws Exception {
+      // given
+
       // when
-      newsCollectScheduler.collect();
+      scheduler.collect();
 
       // then
-      verify(newsCollectService).collect();
+      ArgumentCaptor<JobParameters> captor = ArgumentCaptor.forClass(JobParameters.class);
+      then(jobLauncher).should().run(eq(newsCollectJob), captor.capture());
+      assertThat(captor.getValue().getParameters()).containsKey("time");
     }
   }
 }
