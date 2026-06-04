ad75bf3ee2c153b7e4a969a74e38b4149c249d50
김명근 <rlaaudrms369@gmail.com>
2026-06-04 19:38:19 +0900

    test(green): upload() 메서드 구현

--- src/main/java/com/sprint/mission/monew/batch/LogBackupScheduler.java
+++ src/main/java/com/sprint/mission/monew/batch/LogBackupScheduler.java
@@ -1,7 +1,12 @@
 package com.sprint.mission.monew.batch;
 
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,10 +17,21 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class LogBackupScheduler {
 
-  private final LogBackupService logBackupService;
+  private final JobLauncher jobLauncher;
+
+  private final Job logBackupJob;
 
   @Scheduled(cron = "${scheduler.log-upload.cron}")
-  public void uploadLogs() {
+  public void uploadLogs() throws Exception {
+    log.info("로그 백업 배치 시작");
+
+    JobParameters params = new JobParametersBuilder()
+        .addLong("time", Instant.now().toEpochMilli())
+        .toJobParameters();
+
+    jobLauncher.run(logBackupJob, params);
+
+    log.info("로그 백업 배치 완료");
 
   }
 }
