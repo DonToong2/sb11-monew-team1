acd912c11cfdfa61e9ca0101f7dbcaa4246322da
김명근 <rlaaudrms369@gmail.com>
2026-06-04 12:11:43 +0900

    test(green): collect() 메서드 구현

--- src/main/java/com/sprint/mission/monew/batch/NewsCollectScheduler.java
+++ src/main/java/com/sprint/mission/monew/batch/NewsCollectScheduler.java
@@ -1,7 +1,12 @@
 package com.sprint.mission.monew.batch;
 
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,11 +17,17 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class NewsCollectScheduler {
 
-  private final NewsCollectService newsCollectService;
+  private final JobLauncher jobLauncher;
+  private final Job newsCollectJob;
 
   @Scheduled(cron = "${scheduler.news-collect.cron}")
-  public void collect() {
+  public void collect() throws Exception {
     log.info("뉴스 수집 배치 시작");
+    JobParameters params = new JobParametersBuilder()
+        .addLong("time", Instant.now().toEpochMilli())
+        .toJobParameters();
+
+    jobLauncher.run(newsCollectJob, params);
     log.info("뉴스 수집 배치 완료");
   }
 }
