1054be1498a3543a90874dc513951df72f267ff8
김명근 <rlaaudrms369@gmail.com>
2026-06-04 19:14:15 +0900

    feat: job, step을 위한 Config 클래스 생성, job(), step() Stub 메서드 작성

---
+++ src/main/java/com/sprint/mission/monew/common/config/LogBackupConfig.java
@@ -0,0 +1,40 @@
+package com.sprint.mission.monew.common.config;
+
+import com.sprint.mission.monew.batch.dto.UploadPayload;
+import com.sprint.mission.monew.batch.processor.LogBackupProcessor;
+import com.sprint.mission.monew.batch.reader.LogBackupReader;
+import com.sprint.mission.monew.batch.writer.LogBackupWriter;
+import java.nio.file.Path;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class LogBackupConfig {
+
+  private final JobRepository jobRepository;
+  private final PlatformTransactionManager transactionManager;
+
+  private final LogBackupReader logBackupReader;
+  private final LogBackupProcessor logBackupProcessor;
+  private final LogBackupWriter logBackupWriter;
+
+  @Value("${batch.log-backup.chunk-size}")
+  private int chunkSize;
+
+  @Bean
+  public Job logBackJob() {
+    return null
+  }
+
+  @Bean
+  public Step logBackupStep() {
+    return null
+  }
+}
