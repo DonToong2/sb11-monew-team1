131c2035c7da5705f05681cd43e889bcf043f478
김명근 <rlaaudrms369@gmail.com>
2026-06-04 19:37:55 +0900

    test(red): 정상적인 upload() 테스트 메서드 작성

--- src/test/java/com/sprint/mission/monew/batch/LogBackupSchedulerTest.java
+++ src/test/java/com/sprint/mission/monew/batch/LogBackupSchedulerTest.java
@@ -1,22 +1,34 @@
 package com.sprint.mission.monew.batch;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
 
 @ExtendWith(MockitoExtension.class)
 class LogBackupSchedulerTest {
 
-  @InjectMocks
-  LogBackupScheduler logBackupScheduler;
   @Mock
-  LogBackupService logBackupService;
+  private JobLauncher jobLauncher;
+
+  @Mock
+  private Job logBackupJob;
+
+  @InjectMocks
+  private LogBackupScheduler logBackupScheduler;
 
   @Nested
   @DisplayName("로그 파일 S3 업로드 스케줄러")
@@ -24,12 +36,14 @@ class LogBackupSchedulerTest {
 
     @Test
     @DisplayName("uploadLogs 호출 시 LogBackupService에 위임한다")
-    void uploadLogs_호출_시_서비스에_위임한다() {
+    void uploadLogs_호출_시_서비스에_위임한다() throws Exception {
       // when
       logBackupScheduler.uploadLogs();
 
       // then
-      verify(logBackupService).upload();
+      ArgumentCaptor<JobParameters> captor = ArgumentCaptor.forClass(JobParameters.class);
+      then(jobLauncher).should().run(eq(logBackupJob), captor.capture());
+      assertThat(captor.getValue().getParameters()).containsKey("time");
     }
   }
 }
