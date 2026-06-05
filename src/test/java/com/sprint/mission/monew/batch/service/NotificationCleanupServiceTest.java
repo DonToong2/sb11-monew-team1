ae7501e7994c1cc1464e4aed5c3f6e65b4ef1d65
김명근 <rlaaudrms369@gmail.com>
2026-06-06 01:29:19 +0900

    chore: DisplayName 올바르게 수정

--- src/test/java/com/sprint/mission/monew/batch/service/NotificationCleanupServiceTest.java
+++ src/test/java/com/sprint/mission/monew/batch/service/NotificationCleanupServiceTest.java
@@ -46,7 +46,7 @@ class NotificationCleanupServiceTest {
   }
 
   @Test
-  @DisplayName("logBackupJob이 JobLauncher를 통해 정상 실행된다")
+  @DisplayName("notificationCleanupJob이 JobLauncher를 통해 정상 실행된다")
   void executeBackup_success() throws Exception {
 
     // given
