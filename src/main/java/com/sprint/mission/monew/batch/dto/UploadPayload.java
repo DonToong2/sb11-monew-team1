cf84f681590f0962acd40f2919ae20386a45932a
김명근 <rlaaudrms369@gmail.com>
2026-06-05 18:53:20 +0900

    refactor: byte[] 타입 불변성 보장 로직 추가

--- src/main/java/com/sprint/mission/monew/batch/dto/UploadPayload.java
+++ src/main/java/com/sprint/mission/monew/batch/dto/UploadPayload.java
@@ -7,5 +7,13 @@ public record UploadPayload(
     String s3Key,
     byte[] compressedData
 ) {
+  public UploadPayload {
+    compressedData = compressedData == null ? null: compressedData.clone();
+  }
+
+  @Override
+  public byte[] compressedData() {
+    return compressedData == null ? null : compressedData.clone();
+  }
 
 }
