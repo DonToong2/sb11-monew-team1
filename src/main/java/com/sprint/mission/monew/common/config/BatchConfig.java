fc0deac863158b7d9f5643abd9fb43ec4069330b
김명근 <rlaaudrms369@gmail.com>
2026-06-05 18:10:40 +0900

    feat: BatchConfig 추가

---
+++ src/main/java/com/sprint/mission/monew/common/config/BatchConfig.java
@@ -0,0 +1,10 @@
+package com.sprint.mission.monew.common.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+}
