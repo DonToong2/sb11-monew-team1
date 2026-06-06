b4e8b4a9cd2bdb6d22fa41c140382032a00885f5
김명근 <rlaaudrms369@gmail.com>
2026-06-07 01:14:18 +0900

    refactor: 누락된 로깅 메시지 추가

--- src/main/java/com/sprint/mission/monew/batch/reader/LogBackupReader.java
+++ src/main/java/com/sprint/mission/monew/batch/reader/LogBackupReader.java
@@ -3,11 +3,13 @@ package com.sprint.mission.monew.batch.reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @StepScope
 @Component
 public class LogBackupReader implements ItemReader<Path> {
@@ -31,6 +33,7 @@ public class LogBackupReader implements ItemReader<Path> {
     Path file = Path.of(logDir, "monew." + yesterday + ".log");
 
     if (!Files.exists(file)) {
+      log.warn("로그 파일 없음: {}", file);
       return null;
     }
     return file;
