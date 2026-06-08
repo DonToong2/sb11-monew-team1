a0c27dbf8546b2a034fac190766178196f3680e0
김명근 <rlaaudrms369@gmail.com>
2026-06-08 16:13:34 +0900

    refactor: 잘못 상속받고있는 클래스를 올바르게 수정

--- src/main/java/com/sprint/mission/monew/domain/interest/exception/SubscriptionNotFoundException.java
+++ src/main/java/com/sprint/mission/monew/domain/interest/exception/SubscriptionNotFoundException.java
@@ -1,11 +1,10 @@
 package com.sprint.mission.monew.domain.interest.exception;
 
-import com.sprint.mission.monew.common.exception.MonewException;
 import java.util.Map;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
 
-public class SubscriptionNotFoundException extends MonewException {
+public class SubscriptionNotFoundException extends InterestException {
 
   private SubscriptionNotFoundException(Map<String, Object> details) {
     super(HttpStatus.NOT_FOUND, InterestErrorCode.SUBSCRIPTION_NOT_FOUND, details);
