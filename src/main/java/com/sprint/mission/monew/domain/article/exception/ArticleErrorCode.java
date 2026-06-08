f9dd3a7a0e163f02f3b28d446c9fda8198bde185
김명근 <rlaaudrms369@gmail.com>
2026-06-08 19:34:10 +0900

    refactor: 더 이상 사용되지 않는 예외 클래스 삭제

--- src/main/java/com/sprint/mission/monew/domain/article/exception/ArticleErrorCode.java
+++ src/main/java/com/sprint/mission/monew/domain/article/exception/ArticleErrorCode.java
@@ -8,8 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ArticleErrorCode implements ErrorCode {
 
-  ARTICLE_NOT_FOUND("뉴스 기사를 찾을 수 없습니다."),
-  ARTICLE_INVALID_CURSOR("커서 값의 형식이 올바르지 않습니다.");
+  ARTICLE_NOT_FOUND("뉴스 기사를 찾을 수 없습니다.");
 
   private final String message;
 
