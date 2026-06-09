76d42fda834be13a25e429bed5b14f19f5ba22a6
김명근 <rlaaudrms369@gmail.com>
2026-06-09 19:11:12 +0900

    chore: 더 이상 사용하지 않는 패키지 import문 삭제

--- src/test/java/com/sprint/mission/monew/batch/CommentCleanupJobIntegrationTest.java
+++ src/test/java/com/sprint/mission/monew/batch/CommentCleanupJobIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.sprint.mission.monew.batch;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 
 import com.sprint.mission.monew.domain.article.entity.Article;
 import com.sprint.mission.monew.domain.article.entity.ArticleSource;
