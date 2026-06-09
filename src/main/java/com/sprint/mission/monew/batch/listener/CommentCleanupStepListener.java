3200345a1163bbfe8c7621d14098f579e5e995a4
김명근 <rlaaudrms369@gmail.com>
2026-06-09 18:26:25 +0900

    feat: metrics 지표 확인을 위한 StepExecutionListener 구현체 생성, 오버라이딩한 afterStep() Stub 메서드 작성

---
+++ src/main/java/com/sprint/mission/monew/batch/listener/CommentCleanupStepListener.java
@@ -0,0 +1,21 @@
+package com.sprint.mission.monew.batch.listener;
+
+import com.sprint.mission.monew.batch.metrics.CommentCleanupMetrics;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentCleanupStepListener implements StepExecutionListener {
+
+  private final CommentCleanupMetrics commentCleanupMetrics;
+
+  @Override
+  public ExitStatus afterStep(StepExecution stepExecution) {
+    return null;
+  }
+
+}
