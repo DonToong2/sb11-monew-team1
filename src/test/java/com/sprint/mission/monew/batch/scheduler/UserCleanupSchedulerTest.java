2949084b30494cadc4af5fc15699e0b0d6c165bf
김명근 <rlaaudrms369@gmail.com>
2026-06-09 16:48:03 +0900

    refactor: domain → batch 패키지로 이동

--- src/test/java/com/sprint/mission/monew/batch/scheduler/UserCleanupSchedulerTest.java
+++ src/test/java/com/sprint/mission/monew/batch/scheduler/UserCleanupSchedulerTest.java
@@ -1,8 +1,7 @@
-package com.sprint.mission.monew.domain.user.scheduler;
+package com.sprint.mission.monew.batch.scheduler;
 
 import static org.mockito.BDDMockito.then;
 
-import com.sprint.mission.monew.batch.scheduler.UserCleanupScheduler;
 import com.sprint.mission.monew.batch.service.UserCleanupService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
