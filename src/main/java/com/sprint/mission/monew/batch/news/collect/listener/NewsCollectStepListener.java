45404ef4c7e9078d7e0e239ef7029f59b250ca06
김명근 <rlaaudrms369@gmail.com>
2026-06-10 13:54:27 +0900

    refactor: 로깅 메시지 추가

--- src/main/java/com/sprint/mission/monew/batch/news/collect/listener/NewsCollectStepListener.java
+++ src/main/java/com/sprint/mission/monew/batch/news/collect/listener/NewsCollectStepListener.java
@@ -4,11 +4,13 @@ import com.sprint.mission.monew.batch.news.collect.metrics.NewsCollectMetrics;
 import java.time.Duration;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class NewsCollectStepListener implements StepExecutionListener {
@@ -24,6 +26,8 @@ public class NewsCollectStepListener implements StepExecutionListener {
     );
 
     newsCollectMetrics.recordCollectDuration(duration);
+
+    log.info("News Collect Step 완료 | duration={}", duration);
 
     return stepExecution.getExitStatus();
   }
