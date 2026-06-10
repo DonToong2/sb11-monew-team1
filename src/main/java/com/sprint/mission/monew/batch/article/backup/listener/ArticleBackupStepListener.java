12dfe1130b67c1024ef5caceabd3f68fa32dad8d
김명근 <rlaaudrms369@gmail.com>
2026-06-10 16:13:46 +0900

    refactor: 로깅 메시지 추가

--- src/main/java/com/sprint/mission/monew/batch/article/backup/listener/ArticleBackupStepListener.java
+++ src/main/java/com/sprint/mission/monew/batch/article/backup/listener/ArticleBackupStepListener.java
@@ -4,11 +4,13 @@ import com.sprint.mission.monew.batch.article.backup.metrics.ArticleBackupMetric
 import java.time.Duration;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ArticleBackupStepListener implements StepExecutionListener {
@@ -24,6 +26,8 @@ public class ArticleBackupStepListener implements StepExecutionListener {
     );
 
     articleBackupMetrics.recordDuration(duration);
+
+    log.info("Article Backup Step 완료 | duration={}", duration);
 
     return stepExecution.getExitStatus();
   }
