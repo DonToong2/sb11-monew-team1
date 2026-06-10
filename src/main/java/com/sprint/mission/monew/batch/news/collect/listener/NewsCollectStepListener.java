7263cbf9bbfd475e1ed48455f195eb67db5c6db9
김명근 <rlaaudrms369@gmail.com>
2026-06-10 13:53:00 +0900

    test(green): afterStep() 메서드 구현

--- src/main/java/com/sprint/mission/monew/batch/news/collect/listener/NewsCollectStepListener.java
+++ src/main/java/com/sprint/mission/monew/batch/news/collect/listener/NewsCollectStepListener.java
@@ -1,14 +1,31 @@
 package com.sprint.mission.monew.batch.news.collect.listener;
 
+import com.sprint.mission.monew.batch.news.collect.metrics.NewsCollectMetrics;
+import java.time.Duration;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class NewsCollectStepListener implements StepExecutionListener {
+
+  private final NewsCollectMetrics newsCollectMetrics;
 
   @Override
   public ExitStatus afterStep(StepExecution stepExecution) {
-    return null;
+
+    Duration duration = Duration.between(
+        Objects.requireNonNull(stepExecution.getStartTime()),
+        Objects.requireNonNull(stepExecution.getEndTime())
+    );
+
+    newsCollectMetrics.recordCollectDuration(duration);
+
+    return stepExecution.getExitStatus();
   }
 
 }
